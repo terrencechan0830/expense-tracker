0298c5f18608854bf73248227ba4d36d47e8edc2
Terrence Chan <terrencechan0830@gmail.com>
2023-06-14 01:45:42 +0800

    22:08

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { db } = require('./db/db');
+const { readdirSync } = require('fs');
 const app = express();
 
 require('dotenv').config()
@@ -10,11 +12,11 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
+    db()
     app.listen(PORT, () => {
         console.log('listening to port: ', PORT)
     })
