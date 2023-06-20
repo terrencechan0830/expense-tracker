c9143d5294a89f5eb58478fca186f36e12e51d1e
Terrence Chan <terrence.tf.chan@pccw.com>
2023-06-20 13:22:15 +0800

    3:00:00

--- frontend/src/components/Incomes/Incomes.js
+++ frontend/src/components/Incomes/Incomes.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { styled } from 'styled-components'
 import { InnerLayout } from '../../style/Layouts';
 import { useGlobalContext } from '../../context/globalContext';
 import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Incomes() {
-  const {addIncome} = useGlobalContext()
+  const {addIncome, getIncomes, incomes} = useGlobalContext()
+
+  useEffect(() => {
+    getIncomes()
+  }, [incomes])
+
   return (
     <IncomesStyled>
       <InnerLayout>
@@ -15,7 +21,21 @@ function Incomes() {
             <Form />
           </div>
           <div className='incomes'>
-
+            {incomes.map((income) => {
+              const {_id, title, amount, date, category, description, type} = income;
+              return <IncomeItem
+                key={_id}
+                id={_id}
+                title={title} 
+                description={description} 
+                amount={amount} 
+                date={date} 
+                type={type}
+                category={category} 
+                indicatorColor="var(--color-green)"
+                // deleteItem={deleteIncome}
+              />
+            })}
           </div>
         </div>
       </InnerLayout>
@@ -24,7 +44,15 @@ function Incomes() {
 }
 
 const IncomesStyled = styled.div`
-
+  display: flex;
+  overflow: auto;
+  .income-content{
+    display: flex;
+    gap: 2rem;
+    .incomes{
+      flex: 1;
+    }
+  }
 `;
 
 export default Incomes
